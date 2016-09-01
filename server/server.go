e894dda892d6e9c397f730ad8c90994efaacbe14
Rubén Soleto Buenvarón <ruben.soleto.buenvaron@tecsisa.com>
2016-09-01 10:37:39 +0200

    Changed time expirations

--- server/server.go
+++ server/server.go
@@ -491,7 +491,7 @@ func (s *Server) ClientCredsToken(creds oidc.ClientCredentials) (*jose.JWT, erro
 	}
 
 	now := time.Now()
-	exp := now.Add(s.SessionManager.ValidityWindow)
+	exp := now.Add(session.TokenValidityWindow)
 	claims := oidc.NewClaims(s.IssuerURL.String(), creds.ID, creds.ID, now, exp)
 	claims.Add("name", creds.ID)
 
@@ -660,7 +660,7 @@ func (s *Server) RefreshToken(creds oidc.ClientCredentials, scopes scope.Scopes,
 	}
 
 	now := time.Now()
-	expireAt := now.Add(session.DefaultSessionValidityWindow)
+	expireAt := now.Add(session.TokenValidityWindow)
 
 	claims := oidc.NewClaims(s.IssuerURL.String(), usr.ID, creds.ID, now, expireAt)
 	usr.AddToClaims(claims)
